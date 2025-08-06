01f061e1949690f98b013eba4f92c356113f71d0
bansaltanishq8 <bansaltanishq8@gmail.com>
2025-08-06 14:26:35 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,18 @@
+package Programming_Challenges;
+import java.util.Scanner;
+public class Book {
+    static int total;
+    String author;
+    String title;
+    String isbn;
+    boolean isBorrowed;
+
+    static{
+        total = 0;
+    }
+    Book (String isbn, String title, String author){
+        this.isbn = "f";
+
+    }
+
+}
