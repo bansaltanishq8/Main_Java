c4b65ff0c471ff7fd22e8bb61d45013c626cf44c
bansaltanishq8 <bansaltanishq8@gmail.com>
2025-08-06 16:44:35 +0530

    EX2

--- src/Programming_Challenges/Book.java
+++ src/Programming_Challenges/Book.java
@@ -10,10 +10,45 @@ public class Book {
     static{
         total = 0;
     }
+    {
+        total++;
+    }
     Book (String isbn, String title, String author){
-        this.isbn = "f";
-        this.isbn = "f";
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+
+    }
+    Book (String isbn){
+        this(isbn, "Unknown" , "Unknown");
+
+    }
+    static int GetTotalNoOfBooks() {
+        return total;
+    }
+    void Borrowbook(){
+        if(isBorrowed) {
+            System.out.println("Book is already borrowed!");
+        }
+        else {
+            this.isBorrowed = true;
+            System.out.println("Enjoy ht ");
+        }
+    }
+    void ReturnBook() {
+         if (isBorrowed) {
+         this.isBorrowed = false;
+             System.out.println("Hope you enjoyed the book, Please leave a Review!!");
+         }
+
 
     }
 
 }
+
+
+
+
+
+
+
