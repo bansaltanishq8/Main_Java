79f5aa4b8595e72c90bc4fa869c0fe50f9323423
bansaltanishq8 <bansaltanishq8@gmail.com>
2025-08-06 14:29:04 +0530

    exp

---
+++ src/Programming_Challenges/Book.java
@@ -0,0 +1,19 @@
+package Programming_Challenges;
+import java.util.Scanner;
+public class Book {
+    static int total;
+    String author;
+    String title;
+    String isbn;
+    boolean isBorrowed;
+
+    static{
+        total = 0;
+    }
+    Book (String isbn, String title, String author){
+        this.isbn = "f";
+        this.isbn = "f";
+
+    }
+
+}
